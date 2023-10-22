d7394bb35252328bbb93a1a2d4dde8ff023c2430
Leonardo Lopes <leonardo.lopesc03@gmail.com>
2023-10-22 11:08:59 -0300

    Funções JS

--- Funcoes/index.js
+++ Funcoes/index.js
@@ -15,17 +15,18 @@ function calculoAumento(nome, salario) {
     var aumento = 0
 
     if (salario <= 1500.00) {
-        aumento = 0.2;
+        aumento = 0.2; //20%
     } else if (salario > 1500.01 && salario <= 2000.00) {
-        aumento = 0.15;
+        aumento = 0.15; //15%
     } else if (salario > 2000.01 && salario <= 3000.00) {
-        aumento = 0.1;
+        aumento = 0.1; //10%
     } else {
-        aumento = 0.05;
+        aumento = 0.05; //5%
     }
 
     var novoSalario = salario + (salario * aumento)
-
+    
+    //EXIBINDO RESULTADOS NO CONSOLE
     console.log(`Olá colaborador ${nome}, seu salário inicialmente era equivalente a R$${salario}, após o reajuste de ${aumento * 100}% seu novo salário está estimado em R$${novoSalario}. `)
 
     inserirNovamente()
@@ -34,7 +35,7 @@ function calculoAumento(nome, salario) {
 function inserirNovamente() {
     let opcao = prompt('Deseja calcular o reajuste de outro colaborador? 1 - Sim // 2 - Não')
     if (opcao == '1') {
-        perguntaColaborador();
-    } else { alert('Programa encerrado.') }
+        perguntaColaborador(); //REPETIR PROCESSO DE CALCULO DE REAJUSTE
+    } else { alert('Programa encerrado.') }//FIM DO PROGRAMA
 
 }
